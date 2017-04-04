07bc7ff77ed65c9ba5b68f3e106e1ee6ca9b827a
Brandon Gillespie <brandon.gillespie08@gmail.com>
2017-04-03 23:22:25 -0700

    Merging with current master

--- JavaSource/com/timelink/controllers/LabourReportController.java
+++ JavaSource/com/timelink/controllers/LabourReportController.java
@@ -35,7 +35,7 @@ public class LabourReportController implements Serializable {
   private Project selectedProject;
   private WorkPackage selectedWorkPackage;
   private int projectId;
-  private int workPackageId;
+  private Integer workPackageId;
   
   /**
    * Report the selectedProject.
@@ -89,7 +89,7 @@ public class LabourReportController implements Serializable {
    * Returns the workPackageId.
    * @return the workPackageId
    */
-  public int getWorkPackageId() {
+  public Integer getWorkPackageId() {
     return workPackageId;
   }
 
@@ -97,10 +97,12 @@ public class LabourReportController implements Serializable {
    * Sets the workPackageId to workPackageId.
    * @param workPackageId the workPackageId to set
    */
-  public void setWorkPackageId(int workPackageId) {
-    this.workPackageId = workPackageId;
-    selectedWorkPackage = wpm.find(workPackageId);
-    this.selectedProject = selectedWorkPackage.getProject();
+  public void setWorkPackageId(Integer workPackageId) {
+    if (workPackageId != null) {
+      this.workPackageId = workPackageId;
+      selectedWorkPackage = wpm.find(workPackageId);
+      this.selectedProject = selectedWorkPackage.getProject();
+    }
   }
 
   /**
@@ -126,7 +128,8 @@ public class LabourReportController implements Serializable {
    */
   public Date getDate() {
     if (selectedWorkPackage != null) {
-      List<EstimatedWorkPackageWorkDays> el = ewm.getAllWithWorkPackageUniqueDate(selectedWorkPackage);
+      List<EstimatedWorkPackageWorkDays> el = 
+          ewm.getAllWithWorkPackageUniqueDate(selectedWorkPackage);
       Date mostRecent = new Date();
       for (EstimatedWorkPackageWorkDays eh : el) {
         if (eh.getDateCreated().after(mostRecent)) {
