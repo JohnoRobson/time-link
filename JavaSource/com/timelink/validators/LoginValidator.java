6ff553e8f8a69f307f3fc83d41ff7dcc057a6cbd
loganborean <loganborean@gmail.com>
2017-04-08 14:31:16 -0700

    add login validation

---
+++ JavaSource/com/timelink/validators/LoginValidator.java
@@ -0,0 +1,101 @@
+package com.timelink.validators;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import javax.inject.Named;
+
+
+/**
+ * A class for validating input.
+ */
+@Named("LoginValidator")
+@SessionScoped
+public class LoginValidator implements Serializable {
+    
+    public LoginValidator() {}
+
+
+    /**
+     * Validates the password.
+     * @param context
+     * @param componentToValidate
+     * @param value the value to validate.
+     * @throws ValidatorException
+     */
+    public void validatePassword(FacesContext context,
+            UIComponent componentToValidate, Object value)
+            throws ValidatorException {
+        
+
+        String password = (String) value;
+
+        if (password == null || password.length() == 0) {
+            throw new ValidatorException(
+                    new FacesMessage("You must enter a password"));
+        }
+
+        if (password.length() < 5 || password.length() > 20) {
+            System.out.println("passwrod exception thrown");
+            throw new ValidatorException(
+                    new FacesMessage("password must be between 5 "
+                            + "and 20 characters"));
+        }
+
+        for (int i = 0; i < password.length(); i++) {
+            if (!Character.isLetter(password.charAt(i))
+                    && !Character.isDigit(password.charAt(i))
+                    && password.charAt(i) != '_') {
+
+                throw new ValidatorException(
+                        new FacesMessage("Only alphabetic, numeric "
+                                + "and \'_\' characters are "
+                                + "allowed in password"));
+            }
+        }
+    }
+
+    /**
+     * Validates the username.
+     * @param context
+     * @param componentToValidate
+     * @param value the value to validate.
+     * @throws ValidatorException
+     */
+    public void validateUsername(FacesContext context,
+            UIComponent componentToValidate, Object value)
+            throws ValidatorException {
+
+        String username = (String) value;
+
+        if (username == null || username.length() == 0) {
+            throw new ValidatorException(
+                    new FacesMessage("You must enter a username"));
+        }
+
+        if (username.length() < 5 || username.length() > 20) {
+            System.out.println("username exceptin thrownn");
+            throw new ValidatorException(
+                    new FacesMessage("username must"
+                            + " be between 5 and 20 characters"));
+        }
+
+        for (int i = 0; i < username.length(); i++) {
+            if (!Character.isLetter(username.charAt(i))
+                    && !Character.isDigit(username.charAt(i))
+                    && username.charAt(i) != '_') {
+                throw new ValidatorException(
+                        new FacesMessage("Only alphabetic, "
+                                + "numeric and \'_\' characters "
+                                + "are allowed in username"));
+            }
+        }
+
+    }
+
+
+}
